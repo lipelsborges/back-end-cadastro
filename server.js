e487a90534014908d6c99e3fb690d739593f3e5b
Felipe Borges <lipelsborges@gmail.com>
2025-09-24 18:01:58 -0300

    aa

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 import express from 'express'
+import { PrismaClient }from '@prisma/client';
+
+const prisma = new PrismaClient()
+
 
 const app = express()
+app.use(express.json())
 
-app.get ('/usuario', (req, res) => {
-        res.send("Ok, deu bom")
+const users = []
+
+app.post ('/usuarios' , async (req, res) => {
+        
+       await prisma.user.create({
+                data: {
+                   email: req.body.email,
+                   name: req.body.name,
+                   age: req.body.age,
+                }
+        })
+
+        res.status(201).json(req.body)
+})
+
+app.get ('/usuarios', (req, res) => {
+        res.status(200).json(users)
 
 })
 
 app.listen(3000)
 
+
+/*
+  rodolfo
+  o3oq7OCdWBdPQ9S9
+
+*/
 
